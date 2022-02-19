ea2773d4a8199c044b0f2f1ce89acfbad5809c5e
Timson <timson.robl@gmail.com>
2022-02-19 22:40:28 +0300

    version bump 0.1.26

--- InstaTools.user.js
+++ InstaTools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         InstaTools
 // @namespace    http://tampermonkey.net/
-// @version      0.1.25
+// @version      0.1.26
 // @description  Social network enhancements for power users
 // @author       Timsonrobl
 // @updateURL    https://github.com/Timsonrobl/InstaTools/raw/master/InstaTools.user.js
